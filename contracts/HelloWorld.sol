4662a0e34fba7dcb1a8c5b61cf170afdc3b985ed
Nishant Pai <nishantpai@Nishants-MacBook-Pro.local>
2023-08-06 17:35:10 +0530

    add signing and conditions

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -3,19 +3,17 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 struct Card {
-  uint256 id;
-  string message;
-  mapping(address => bool) signed;
-  string data;
+    uint256 id;
+    string message;
+    string data;
 }
 
 contract HelloWorld is ERC1155 {
-
     // CARDS
     Card[] private _cards;
-    
-    constructor() ERC1155("") {
-    }
+    mapping(uint256 => mapping(address => bool)) private _signedStatus;
+
+    constructor() ERC1155("") {}
 
     // MINT CARD TO ADDRESS
     function mint(address account, string memory message) public {
@@ -27,9 +25,11 @@ contract HelloWorld is ERC1155 {
     // SIGN CARD
     function sign(uint256 id, string memory name) public {
         require(id < _cards.length, "Card does not exist");
-        require(!_cards[id][msg.sender], "Card already signed");
-        _cards[id].message = string(abi.encodePacked(_cards[id].message, " - ", name));
-        _cards[id][msg.sender] = true;
+        require(!_signedStatus[id][msg.sender], "Card already signed");
+        _cards[id].message = string(
+            abi.encodePacked(_cards[id].message, " - ", name)
+        );
+        _signedStatus[id][msg.sender] = true;
     }
 
     function uri(uint256 id) public view override returns (string memory) {
